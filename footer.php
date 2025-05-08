ab853cd280464ce8ca95bc8b086b631bf5b20f1d
prabhats-github <githubprabhat@gmail.com>
2025-05-08 10:50:20 +0530

    Initial commit: Upload full project

--- footer.php
+++ footer.php
@@ -1,40 +1,43 @@
 <footer class="bg-secondary pt-6 pt-lg-8">
-            <div class="container">
-                <div class="row mt-n2-9 pb-6 pb-lg-8">
-                    <div class="col-md-6 col-lg-5 mt-2-9 wow fadeIn" data-wow-delay="200ms">
-                        <div class="footer-logo mb-1-9">
-                            <a href="index.php"><img src="img/logos/logo-inner.png" alt="..."></a>
+    <div class="container">
+        <div class="row mt-n2-9 pb-6 pb-lg-8">
+            <div class="col-md-6 col-lg-5 mt-2-9 wow fadeIn" data-wow-delay="200ms">
+                <div class="footer-logo mb-1-9">
+                    <a href="index.php"><img src="img/logos/logo-inner.png" alt="..."></a>
+                </div>
+                <h4 class="text-white mb-1-9 fw-light w-lg-75 display-27 lh-base opacity8">We have been most trusted web
+                    design agency since 25+ years.</h4>
+                <ul class="social-icon-style1">
+                    <li>
+                        <a href="https://www.facebook.com/auricsoft06?_rdr"><i class="fab fa-facebook-f"></i></a>
+                    </li>
+                    <li>
+                        <a href="https://x.com/auricsoft06/status/1376205341059207171"><i
+                                class="fab fa-twitter"></i></a>
+                    </li>
+                    <li>
+                        <a href="https://www.youtube.com/@myauricsoft/videos"><i class="fab fa-youtube"></i></a>
+                    </li>
+                    <li>
+                        <a href="https://in.linkedin.com/company/auricsoft06"><i class="fab fa-linkedin-in"></i></a>
+                    </li>
+                </ul>
+            </div>
+            <div class="col-md-6 col-lg-3 mt-2-9 wow fadeIn" data-wow-delay="400ms">
+
+                <div class="ps-xl-4">
+                    <h3 class="text-white h4 mb-1-9">Reach Us</h3>
+                    <div class="d-flex mb-1-9">
+                        <div class="flex-shrink-0">
+                            <img src="img/icons/icon-phone.png" alt="...">
                         </div>
-                        <h4 class="text-white mb-1-9 fw-light w-lg-75 display-27 lh-base opacity8">We have been most trusted web design agency since 25+ years.</h4>
-                        <ul class="social-icon-style1">
-                            <li>
-                                <a href="https://www.facebook.com/auricsoft06?_rdr"><i class="fab fa-facebook-f"></i></a>
-                            </li>
-                            <li>
-                                <a href="https://x.com/auricsoft06/status/1376205341059207171"><i class="fab fa-twitter"></i></a>
-                            </li>
-                            <li>
-                                <a href="https://www.youtube.com/@myauricsoft/videos"><i class="fab fa-youtube"></i></a>
-                            </li>
-                            <li>
-                                <a href="https://in.linkedin.com/company/auricsoft06"><i class="fab fa-linkedin-in"></i></a>
-                            </li>
-                        </ul>
+                        <div class="flex-grow-1 ms-3">
+
+                            <p class="mb-0 text-white">E 44/3, Pocket D, Okhla Phase II, Okhla Industrial Area, New
+                                Delhi, Delhi 110020</p>
+                        </div>
                     </div>
-                    <div class="col-md-6 col-lg-3 mt-2-9 wow fadeIn" data-wow-delay="400ms">
-                                           
-                        <div class="ps-xl-4">
-                            <h3 class="text-white h4 mb-1-9">Reach Us</h3>
-                            <div class="d-flex mb-1-9">
-                                <div class="flex-shrink-0">
-                                    <img src="img/icons/icon-phone.png" alt="...">
-                                </div>
-                                <div class="flex-grow-1 ms-3">
-                                    
-                                    <p class="mb-0 text-white">E 44/3, Pocket D, Okhla Phase II, Okhla Industrial Area, New Delhi, Delhi 110020</p>
-                                </div>
-                            </div>
-                            <!-- <div class="d-flex mb-1-9">
+                    <!-- <div class="d-flex mb-1-9">
                                 <div class="flex-shrink-0">
                                     <img src="img/icons/icon-phone.png" alt="...">
                                 </div>
@@ -43,43 +46,43 @@
                                     <p class="mb-0 text-white"> 1st Floor, 199, Saket, Opp Nehru Stadium, Roorkee - 247667</p>
                                 </div>
                             </div> -->
-                        </div>
-                    </div>
-                
-                    <div class="col-md-6 col-lg-3 mt-2-9 offset-lg-1 wow fadeIn" data-wow-delay="600ms">
-                        <div class="ps-xl-4">
-                            <h3 class="text-white h4 mb-1-9">Contacts</h3>
-                            <div class="d-flex mb-1-9">
-                                <div class="flex-shrink-0">
-                                    <img src="img/icons/icon-phone.png" alt="...">
-                                </div>
-                                <div class="flex-grow-1 ms-3">
-                                    <h4 class="mb-1 h5 text-white">Contact Us</h4>
-                                    <p class="mb-0 text-white">+91-8650155151</p>
-                                </div>
-                            </div>
-                            <div class="d-flex">
-                                <div class="flex-shrink-0">
-                                    <img src="img/icons/icon-mail.png" alt="...">
-                                </div>
-                                <div class="flex-grow-1 ms-3">
-                                    <h4 class="mb-1 h5 text-white">Mail Us</h4>
-                                    <p class="mb-0 text-white"><a href="mailto:info@auricsoft.com" class="text-white">info@auricsoft.com</a> </p>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
                 </div>
             </div>
-            <div class="py-4 border-top border-color-light-white">
-                <div class="container">
-                    <div class="row align-items-center">
-                        <div class="col-12 text-center">
-                            <p class="d-inline-block text-white mb-0">&copy; <span class="current-year"></span> Auricsoft Platform Private Limited</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-</footer>
 
-    
+            <div class="col-md-6 col-lg-3 mt-2-9 offset-lg-1 wow fadeIn" data-wow-delay="600ms">
+                <div class="ps-xl-4">
+                    <h3 class="text-white h4 mb-1-9">Contacts</h3>
+                    <div class="d-flex mb-1-9">
+                        <div class="flex-shrink-0">
+                            <img src="img/icons/icon-phone.png" alt="...">
+                        </div>
+                        <div class="flex-grow-1 ms-3">
+                            <h4 class="mb-1 h5 text-white">Contact Us</h4>
+                            <p class="mb-0 text-white">+91-8650155151</p>
+                        </div>
+                    </div>
+                    <div class="d-flex">
+                        <div class="flex-shrink-0">
+                            <img src="img/icons/icon-mail.png" alt="...">
+                        </div>
+                        <div class="flex-grow-1 ms-3">
+                            <h4 class="mb-1 h5 text-white">Mail Us</h4>
+                            <p class="mb-0 text-white"><a href="mailto:info@auricsoft.com"
+                                    class="text-white">info@auricsoft.com</a> </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="py-4 border-top border-color-light-white">
+        <div class="container">
+            <div class="row align-items-center">
+                <div class="col-12 text-center">
+                    <p class="d-inline-block text-white mb-0">&copy; <span class="current-year"></span> Auricsoft
+                        Platform Private Limited</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
